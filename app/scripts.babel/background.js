176bca142d94cedf74d8602500831f0d7551582c
Eli Sagy <sagyeli@gmail.com>
2020-05-11 00:54:54 +0300

    Allowing webpage data to received by client side

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,14 @@
 'use strict';
 
-function request(url, method) {
+function request(url, method, body, headers) {
+  headers = headers || {};
   return new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest;
+    let xhr = new XMLHttpRequest;
     xhr.open(method, url, true);
+    for (let header in headers) {
+      xhr.setRequestHeader(header, headers[header]);
+    }
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     xhr.onreadystatechange = function() {
       if (this.readyState == 4) {
         if (this.status >= 200 && this.status < 400) {
@@ -14,13 +19,13 @@ function request(url, method) {
         }
       }
     };
-    xhr.send();
+    xhr.send(JSON.stringify(body));
   });
 }
 
 chrome.runtime.onConnect.addListener(port => {
-  port.onMessage.addListener((request, sender) => {
-    switch (request.function) {
+  port.onMessage.addListener((req, sender) => {
+    switch (req.function) {
       case 'getProfileUserInfo':
         chrome.identity.getProfileUserInfo(profileUserInfo => sender.postMessage(profileUserInfo));
         break;
@@ -32,6 +37,15 @@ chrome.runtime.onConnect.addListener(port => {
           chrome.identity.removeCachedAuthToken({ token });
         });
         break;
+      case 'updateWebpage':
+        chrome.identity.getAuthToken({
+          'interactive': true
+        }, token => {
+          request(`https://www.pagebubble.com/api/webpages/${encodeURIComponent(req.params.url)}`, 'PUT', req.params, {
+            'Authorization': `Bearer ${token}`
+          });
+        });
+        break;
       default:
         sendResponse();
         break;
@@ -41,7 +55,13 @@ chrome.runtime.onConnect.addListener(port => {
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   chrome.pageAction.show(tabId);
-  if (changeInfo.url && !changeInfo.url.match(/^chrome\:/)) {
-    request(`https://www.pagebubble.com/api/webpages/${encodeURIComponent(changeInfo.url)}`, 'PUT');
-  }
+  // if (changeInfo.url && !changeInfo.url.match(/^chrome\:/)) {
+  //   chrome.identity.getAuthToken({
+  //     'interactive': true
+  //   }, token => {
+  //     request(`https://www.pagebubble.com/api/webpages/${encodeURIComponent(changeInfo.url)}`, 'PUT', {}, {
+  //       'Authorization': `Bearer ${token}`
+  //     });
+  //   });
+  // }
 });
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,60 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+function request(url, method, headers) {
+  headers = headers || {};
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest;
+    xhr.open(method, url, true);
+    for (let header in headers) {
+      xhr.setRequestHeader(header, headers[header]);
+    }
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status >= 200 && this.status < 400) {
+          resolve(JSON.parse(this.responseText || '{}'));
+        }
+        if (this.status >= 400 && this.status < 600) {
+          reject(JSON.parse(this.responseText || '{}'));
+        }
+      }
+    };
+    xhr.send();
+  });
+}
+
+var observeDOM = (function() {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+    timer;
+
+  return function(obj, callback) {
+    if (!obj || !obj.nodeType === 1) return; // validation
+
+    if (MutationObserver) {
+      // define a new observer
+      var obs = new MutationObserver(function(mutations, observer) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, 1000, mutations);
+      })
+      // have the observer observe foo for changes in children
+      obs.observe(obj, { childList: true, subtree: true });
+    } else if (window.addEventListener) {
+      obj.addEventListener('DOMNodeInserted', callback, false);
+      obj.addEventListener('DOMNodeRemoved', callback, false);
+    }
+  }
+})();
+
+var urls = [];
+observeDOM(document, () => {
+  for (var url of [...document.querySelectorAll('a[href]')].map(link => link.href)) {
+    if (!urls.includes(url)) {
+      urls.push(url);
+    }
+  }
+  var port = chrome.runtime.connect();
+  port.postMessage({ function: 'updateWebpage', params: { url: location.href, title: ([...document.querySelectorAll('title')].shift() || {}).innerHTML, hrefs: urls.map(url => ({ url })) } });
+  port.onMessage.addListener(response => {
+    resolve(response);
+    port.disconnect();
+  });
+});
